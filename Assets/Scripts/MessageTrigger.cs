579256eb36f290f3204aa1491819764ec64e7007
astoref <francesco1.astore@gmail.com>
2024-02-16 18:39:14 +0100

    Script Message Trigger e Dialogue Manager

--- Assets/Scripts/MessageTrigger.cs
+++ Assets/Scripts/MessageTrigger.cs
@@ -7,6 +7,10 @@ public class MessageTrigging : MonoBehaviour
     // Start is called before the first frame update
     public Message[] messages;
     public Actor[] actors;
+
+    public void StartSialogue(){
+        FindObjectOfType<DialogueManager>().OpenDialogue(messages, actors);
+    }
 }
 [System.Serializable]
 public class Message{
