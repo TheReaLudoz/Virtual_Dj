350aed8f7b6d803a3b09085ae3899d32c302dc5c
astoref <francesco1.astore@gmail.com>
2024-02-16 18:57:25 +0100

    Dialogue Trigger

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MessageTrigging : MonoBehaviour
+public class DialogueTrigger : MonoBehaviour
 {
     // Start is called before the first frame update
     public Message[] messages;
