8da23aeed5f34c41d48c0114394b54a3c260eb38
Michael <Michaelconsigli98@gmail.com>
2024-02-19 17:55:35 +0100

    boh

--- Assets/KnobsAsset/Scripts/KnobListeners/LightKnobListener.cs
+++ Assets/KnobsAsset/Scripts/KnobListeners/LightKnobListener.cs
@@ -25,15 +25,17 @@ namespace KnobsAsset
         [SerializeField] private float MinimumRange = 10f;
         [SerializeField] private float MaximumRange = 100f;
         [SerializeField] private bool AdjustTime = true;
+        [SerializeField] private float MinimumTime = 0.3f;
+        [SerializeField] private float MaximumTime = 3f;
+ 
 
         private void Awake()
         {
-            if (Lights.Length < 1);
+            if (Lights.Length < 1)
             {
                 Debug.LogWarning("No lights assigned, this listener will have no effect", this);
             }
         }
-
 
         public override void OnKnobValueChange(float knobPercentValue)
         {
@@ -56,7 +58,10 @@ namespace KnobsAsset
                 {
                     light.range = range;
                 }
-             
+                if (AdjustTime)
+                {
+                    light.GetComponent<LightScript>().Time = time;
+                }
             }
         }
     }
